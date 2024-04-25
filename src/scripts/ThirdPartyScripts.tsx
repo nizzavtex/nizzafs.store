86a4e4964e3c426e75fc75d417c3a9f29f0ddfda
leidymgdev <martinezgiraldoleidy@gmail.com>
2024-04-24 20:45:17 -0500

    Update!

--- src/scripts/ThirdPartyScripts.tsx
+++ src/scripts/ThirdPartyScripts.tsx
@@ -1,12 +1,12 @@
 import LiveShoppingScript from "./LiveShopping"
 import fastStoreConfig from "../../faststore.config"
 
-const ACCOUNT = fastStoreConfig.api.storeId; 
+const ACCOUNT = fastStoreConfig.api.storeId;
 
 const ThirdPartyScripts = () => {
-    return (
-      <LiveShoppingScript id="99c86792-87b4-4b5b-b965-acc016819d6c" account={ACCOUNT}/>
-    )
-  }
-  
-  export default ThirdPartyScripts
+  return (null
+    // <LiveShoppingScript id="99c86792-87b4-4b5b-b965-acc016819d6c" account={ACCOUNT}/>
+  )
+}
+
+export default ThirdPartyScripts
